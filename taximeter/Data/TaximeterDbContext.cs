db7fe2891b56e7a7507c10f4ec63bc5429b73e36
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-05 13:54:08 -0600

    Feat Builiding the DbContext

---
+++ taximeter/Data/TaximeterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using taximeter.Entity;
+
+namespace taximeter.Data;
+
+public class TaximeterDbContext : DbContext
+{
+    public TaximeterDbContext(DbContextOptions<TaximeterDbContext> options) : base(options) 
+    { 
+
+    }
+
+    public DbSet<Conductor> Conductores { get; set; }
+
+    public DbSet<Taxi> Taxis { get; set; }
+
+    public DbSet<Trayecto> Trayectos { get; set; }
+}
+
