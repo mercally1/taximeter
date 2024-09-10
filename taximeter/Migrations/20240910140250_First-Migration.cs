9b1fea1eaad88a27e35a9aee29d874c311a1c5d7
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-10 08:10:00 -0600

    Contralador y vista de trayecto

---
+++ taximeter/Migrations/20240910140250_First-Migration.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taximeter.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conductores",
+                columns: table => new
+                {
+                    ConductorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Licencia = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Contacto = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conductores", x => x.ConductorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Taxis",
+                columns: table => new
+                {
+                    TaxiId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConductorId = table.Column<int>(type: "int", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Anho = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Taxis", x => x.TaxiId);
+                    table.ForeignKey(
+                        name: "FK_Taxis_Conductores_ConductorId",
+                        column: x => x.ConductorId,
+                        principalTable: "Conductores",
+                        principalColumn: "ConductorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trayectos",
+                columns: table => new
+                {
+                    TrayectoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ubicacion_Inicial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ubicacion_Final = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kilometraje = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaxiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trayectos", x => x.TrayectoId);
+                    table.ForeignKey(
+                        name: "FK_Trayectos_Taxis_TaxiId",
+                        column: x => x.TaxiId,
+                        principalTable: "Taxis",
+                        principalColumn: "TaxiId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Taxis_ConductorId",
+                table: "Taxis",
+                column: "ConductorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trayectos_TaxiId",
+                table: "Trayectos",
+                column: "TaxiId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trayectos");
+
+            migrationBuilder.DropTable(
+                name: "Taxis");
+
+            migrationBuilder.DropTable(
+                name: "Conductores");
+        }
+    }
+}
